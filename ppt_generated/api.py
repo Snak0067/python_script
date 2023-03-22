e6aa173f0c9e51ba2070242df568cb16330bcdcd
Xiaofeng <124476214t5@qq.com>
2023-03-22 11:27:28 +0800

    change ppt code

--- ppt_generated/api.py
+++ ppt_generated/api.py
@@ -19,17 +19,40 @@ def loadcsv():
 
 
 def generate(words: list):
+    """
+    生成ppt文本
+    :param words: ppt内容的字典数据
+    :return:
+    """
     # 创建空白演示文稿
     prs = Presentation()
+    for data in words:
+        slide_layout = prs.slide_layouts[1]
+        slide = prs.slides.add_slide(slide_layout)
+        shapes = slide.shapes
+        # 在指定位置添加文本框
+        textbox = slide.shapes.add_textbox(Cm(3), Cm(5), Cm(10), Cm(10))
+        tf = textbox.text_frame
+        # 在文本框中写入文字
+        para_title = tf.add_paragraph()  # 新增段落
+        para_title.text = data[0]  # 向段落写入文字
+        # 设置字体
+        font = para_title.font
+        font.name = '微软雅黑'  # 字体类型
+        font.bold = True  # 加粗
+        font.size = Pt(64)  # 大小
 
-    slide_layout = prs.slide_layouts[1]
-    slide = prs.slides.add_slide(slide_layout)
-    shapes = slide.shapes
-    for shape in shapes:
-        print(shape.text_frame)
-    text_frame = shape.text_frame
-    para1 = text_frame.paragraphs[0]
-    print(para1)
+        # 在指定位置添加文本框
+        textbox = slide.shapes.add_textbox(Cm(3), Cm(2), Cm(10), Cm(10))
+        tf = textbox.text_frame
+        # 在文本框中写入文字
+        para_title = tf.add_paragraph()  # 新增段落
+        para_title.text = str(data[1])  # 向段落写入文字
+        # 设置字体
+        font = para_title.font
+        font.name = '微软雅黑'  # 字体类型
+        font.bold = True  # 加粗
+        font.size = Pt(64)  # 大小
     # 保存
     prs.save('new_medical_dict.pptx')
 
